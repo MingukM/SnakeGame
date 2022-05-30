e8368e4e3c08d6a9d3ace0e3803846b64fbe2b99
Minguk Moon <minguk971202@gmail.com>
2022-05-30 16:04:08 -0400

    update

--- README.md
+++ README.md
@@ -3,7 +3,12 @@ My very first personal project to build my portfolio.
 ## Goals
 I am interested in making games, so I have decided to make a simple snake game using java.
 
-I will follow the tutorial to start making the game. However, I will be making changes to the code when I am done with the tutorial.
+I have completed the tutorial. 
+
+However, this tutorial does not provide the explanation for the functions that are being used for making the game. It does not explain why it works and how it works. I simply copied the work that was being done on the video.
+
+Therefore, I will be challenging myself to furnish the game. 
+This time I will limit my sources to codes which I already possess and the Java documentation provided on Oracle website.
 
 Also, this project will serve as a tool to teach myself how to use Git and Github. 
 
--- src/GamePanel.java
+++ src/GamePanel.java
@@ -61,23 +61,37 @@ public class GamePanel extends JPanel implements ActionListener {
 	
 	public void draw(Graphics g) {
 		
-		for(int i = 0; i < SCREEN_HEIGHT/UNIT_SIZE; i++) {
-			g.drawLine(i*UNIT_SIZE,0,i*UNIT_SIZE, SCREEN_HEIGHT);
-			g.drawLine(0,i*UNIT_SIZE, SCREEN_HEIGHT, i*UNIT_SIZE);
+		if(running) {
+			/*
+			for(int i = 0; i < SCREEN_HEIGHT/UNIT_SIZE; i++) {
+				g.drawLine(i*UNIT_SIZE,0,i*UNIT_SIZE, SCREEN_HEIGHT);
+				g.drawLine(0,i*UNIT_SIZE, SCREEN_HEIGHT, i*UNIT_SIZE);
+			}*/
+			
+			//Apple
+			g.setColor(Color.red);
+			g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+			
+			//Snake
+			for(int i = 0; i < bodyParts; i++) {
+				if(i == 0) {
+					g.setColor(Color.green);
+					g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+				}
+				else {
+					g.setColor(new Color(random.nextInt(255), random.nextInt(255), random.nextInt(255)));
+					g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+				}
+				
+				//Score
+				g.setColor(Color.red);
+				g.setFont(new Font("Ink Free", Font.BOLD, 40));
+				FontMetrics metrics = getFontMetrics(g.getFont());
+				g.drawString("Score: "+applesEaten, (SCREEN_WIDTH - metrics.stringWidth("Score: "+applesEaten))/2, g.getFont().getSize());
+			}
 		}
-		
-		g.setColor(Color.red);
-		g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
-		
-		for(int i = 0; i < bodyParts; i++) {
-			if(i == 0) {
-				g.setColor(Color.green);
-				g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
-			}
-			else {
-				g.setColor(Color.green);
-				g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
-			}
+		else {
+			gameOver(g);
 		}
 	}
 	
@@ -110,7 +124,11 @@ public class GamePanel extends JPanel implements ActionListener {
 	}
 	
 	public void checkApple() {
-		
+		if((x[0] == appleX) && y[0] == appleY) {
+			bodyParts++;
+			applesEaten++;
+			newApple();
+		}
 	}
 	
 	public void checkCollisions() {
@@ -144,7 +162,16 @@ public class GamePanel extends JPanel implements ActionListener {
 	}
 	
 	public void gameOver(Graphics g) {
-		
+		//Game over text
+		g.setColor(Color.red);
+		g.setFont(new Font("Ink Free", Font.BOLD, 75));
+		FontMetrics metrics = getFontMetrics(g.getFont());
+		g.drawString("Game Over", (SCREEN_WIDTH - metrics.stringWidth("Game Over"))/2, SCREEN_HEIGHT/2);
+	
+		g.setColor(Color.red);
+		g.setFont(new Font("Ink Free", Font.BOLD, 40));
+		FontMetrics metrics2 = getFontMetrics(g.getFont());
+		g.drawString("Score: "+applesEaten, (SCREEN_WIDTH - metrics2.stringWidth("Score: "+applesEaten))/2, g.getFont().getSize());
 	}
 
 	@Override
